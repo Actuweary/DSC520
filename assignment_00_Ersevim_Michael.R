6bf43661a9f141c3ffafa8accec617b81bd3cf2e
Actuweary <85460865+Actuweary@users.noreply.github.com>
2021-09-12 07:45:34 -0400

    Week 2 hmwk

---
+++ assignment_00_Ersevim_Michael.R
@@ -0,0 +1,43 @@
+# Assignment: ASSIGNMENT 0
+# Name: Ersevim, Michael
+# Date: 2021-09-10
+
+# Basics
+
+## Add 8 and 5
+print(8 + 5)
+## Subtract 6 from 22
+print(22 - 6)
+## Multiply 6 by 7
+print(6 * 7)
+## Add 4 to 6 and divide the result by 2
+print((4 + 6)/2)
+## Compute 5 modulo 2
+print(5 %% 2)
+## Assign the value 82 to the variable x
+82 -> x
+## Print x
+print (x)
+## Assign the value 41 to the variable y
+y = 41
+## Print y
+y <- 41
+y
+## Assign the output of x + y to the variable z
+## Print z
+z <- x + y
+z
+## Assign the string value "DSC520" to the variable class_name
+## Print the value of class_name
+class_name <- "DSC520"
+class_name
+## Assign the string value of TRUE to the variable is_good
+## Print the value of is_good
+is_good <- TRUE
+is_good
+## Check the class of the variable is_good using the `class()` function
+class(is_good)
+## Check the class of the variable z using the `class()` function
+class(z)
+## Check the class of the variable class_name using the class() function
+class(class_name)
